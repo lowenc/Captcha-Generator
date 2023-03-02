c13ae8b1c567b56f9b46cb8df72decf3c55780f6
nclow <nclow@Spectre.localdomain>
2023-03-02 14:47:38 -0500

    Commit

--- Assignment07/Src/Assignment07.py
+++ Assignment07/Src/Assignment07.py
@@ -1,8 +1,12 @@
-'''
-Created on Feb 26, 2020
+#Name: Nathan Lowe
+#email: lowenc@mail.uc.edu
+#Assignment Title: Assignment 07
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: Creating a captcha in python
+#Citations: 
+#Anything else that's relevant:
 
-@author: nicomp
-'''
 import random
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
@@ -27,27 +31,39 @@ def draw_random_ellipse(draw):
                                   default_color_blue + random.randrange(-100,100,1), 255), 
                                   outline = "black")
 
-def generate_captcha():
+def generate_captcha(captchaLength, FileName):
     '''
     Generate a captcha
     :return: A tuple (image, captcha string encoded in the image)
     '''
-    captcha_string = generate_random_string(5)
+    # 1A
+    if captchaLength < 6 or captchaLength > 10:
+        captchaLength = 10
+        
+    captcha_string = generate_random_string(captchaLength)
 #   print(">" + captcha_string + "<")
     captcha_image = Image.new("RGBA", (400, 200), (default_color_red,default_color_green,default_color_blue))
     draw = ImageDraw.Draw(captcha_image, "RGBA")
     for i in range(1,20):
         draw_random_ellipse(draw)
+        
 
-    fontStyle = ImageFont.truetype("Aaargh.ttf", 48)     # font must be in the same folder as the .py file. 
 
     # Arbitrary starting co-ordinates for the text we will write
     x = 10 + random.randrange(0, 100, 1)
     y = 79 + random.randrange(-10, 10, 1)
     for letter in captcha_string:
+        # 2
+        fontList = ["AGAALER.ttf", "Aaargh.ttf", "Shadow.ttf"]
+        randomFont = random.choice(fontList)
+        fontStyle = ImageFont.truetype(randomFont, 48)     
 #       print(letter)
         draw.text((x, y), letter, (0,0,0),font=fontStyle)    # Write in black
         x = x + 35
         y = y +  random.randrange(-10, 10, 1)
+        
+    # 1B
+    rgb_im = captcha_image.convert("RGB")
+    rgb_im.save(FileName)
     
     return (captcha_image, captcha_string)  # return a heterogeneous tuple
--- Assignment07/Src/Main.py
+++ Assignment07/Src/Main.py
@@ -1,12 +1,15 @@
-'''
-Created on Feb 26, 2020
-
-@author: nicomp
-'''
+#Name: Nathan Lowe
+#email: lowenc@mail.uc.edu
+#Assignment Title: Assignment 07
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: Creating a captcha in python
+#Citations: 
+#Anything else that's relevant:
 
 from Src.Assignment07 import generate_captcha
 
-result = generate_captcha()
+result = generate_captcha(6, "test.png")
 myCaptcha = result[0]
 captcha_string = result[1]
 print(">" + captcha_string + "<")
